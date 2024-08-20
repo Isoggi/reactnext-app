8af39ae82566032c6d1965df7f637282ca90ac86
Kristanto Nugraha <33407224+Isoggi@users.noreply.github.com>
2024-08-20 18:03:50 +0700

    change .env

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -6,9 +6,11 @@ import React from "react";
 type Props = {};
 
 export default function NavbarComponent({}: Props) {
+  //   const [isDark, setIsDark] = React.useState(false);
+
   return (
     <>
-      <div className="group z-10 items-center lg:justify-between font-mono text-sm lg:flex fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="group z-10 items-center lg:justify-between font-mono text-sm lg:flex fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         <Link
           className="rounded-lg border border-transparent px-2 py-2 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           href={"/"}
@@ -29,7 +31,12 @@ export default function NavbarComponent({}: Props) {
           >
             <code className="font-mono font-bold">Home</code>
           </Link>
-
+          <Link
+            className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            href={"/dashboard"}
+          >
+            <code className="font-mono font-bold">Dashboard</code>
+          </Link>
           <Link
             className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             href={"/login"}
